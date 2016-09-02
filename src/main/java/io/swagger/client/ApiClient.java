0365d2f08a6dfc6bf165353461890a7e1ae12568
itabrezshaikh <itabrezshaikh@gmail.com>
2016-09-02 18:23:51 +0530

    changed user agent to track api calls in webserver log

--- src/main/java/io/swagger/client/ApiClient.java
+++ src/main/java/io/swagger/client/ApiClient.java
@@ -171,7 +171,7 @@ public class ApiClient {
         this.lenientDatetimeFormat = true;
 
         // Set default User-Agent.
-        setUserAgent("Swagger-Codegen/1.0.0/java");
+        setUserAgent("pepipost-sdk-java");
 
         // Setup authentications (key: authentication name, value: authentication).
         authentications = new HashMap<String, Authentication>();
