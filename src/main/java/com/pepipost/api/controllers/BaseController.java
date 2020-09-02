7806cb453cc9932c45bfaa7a4e060d1c24f45b46
snipperbytes <snipperbytes@gmail.com>
2020-09-02 20:29:31 +0530

    updating to user-agent misc

--- src/main/java/com/pepipost/api/controllers/BaseController.java
+++ src/main/java/com/pepipost/api/controllers/BaseController.java
@@ -19,7 +19,7 @@ public abstract class BaseController {
      */
     private static HttpClient clientInstance = null;
     private static final Object syncObject = new Object();
-    protected static final String userAgent = "APIMATIC 2.0";
+    protected static final String userAgent = "pepi-sdk-java-v5.1";
 
     /**
      * Protected variable to keep reference of httpCallBack instance if user provides any
